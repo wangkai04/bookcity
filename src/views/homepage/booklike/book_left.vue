24622f7c06ac672a3ebbf633fba2050ee27392e8
Nosecondminister <1163207469@qq.com>
2018-01-09 08:49:13 +0800

    like

---
+++ src/views/homepage/booklike/book_left.vue
@@ -0,0 +1,94 @@
+<template>
+	<div>
+		<div class="top_lefts">
+			<ul class="list_a">
+				<li>
+					<a href="#"><img src="http://img3m3.ddimg.cn/35/33/23530553-1_l.jpg" /></a>
+					<p class="name"><a href="#">最美的教育最简单</a></p>
+					<p class="tname">
+						<span class="tname_a">
+							<span style="width: 90%;"></span>
+						</span>
+					</p>
+					<p class="price">￥35.5</p>
+				</li>
+				<li>
+					<a href="#"><img src="http://img3m3.ddimg.cn/35/33/23530553-1_l.jpg" /></a>
+					<p class="name"><a href="#">最美的教育最简单</a></p>
+					<p class="tname">
+						<span class="tname_a">
+							<span style="width: 90%;"></span>
+						</span>
+					</p>
+					<p class="price">￥35.5</p>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+  .price{
+  	padding: 0 0 0 23px;
+    height: 16px;
+    line-height: 16px;
+    overflow: hidden;
+    font-size: 14px;
+    display: block;
+    margin-top: 10px;
+  }
+  .tname_a span{
+  	display: block;
+    height: 13px;
+    font-size: 0;
+    width: 69px;
+    background: url(http://book.dangdang.com/Standard/Book/Extend/hosts/images/home/book_star_bg.png) 0 top repeat-x;
+  }
+  .tname_a{
+  	display: inline-block;
+    height: 13px;
+    font-size: 0;
+    width: 69px;
+    background: url(http://book.dangdang.com/Standard/Book/Extend/hosts/images/home/book_star_bg.png) 0 bottom repeat-x;
+    position: relative;
+    top: 1px;
+  }
+  .tname{
+  	height: 14px;
+    padding: 3px 0 3px 22px;
+    margin-top: -20px;
+  }
+  .name{
+  	padding: 7px 23px 0;
+    height: 36px;
+    line-height: 18px;
+    overflow: hidden;
+    font-size: 12px;
+  }
+  .list_a li{
+  	width: 150px;
+    margin-right: 47px;
+    float: left;
+    position: relative;
+    margin-bottom: 17px;
+  }
+  .list_a{
+  	width: 1000px;
+    position: relative;
+    left: 0;
+    margin-left: 20px;
+  }
+  .top_lefts{
+  	width: 960px;
+    float: left;
+    position: relative;
+    overflow: hidden;
+    padding-top: 20px;
+    height: 490px;
+    margin-top: 50px;
+    margin-left: -1200px;
+  }
+</style>
